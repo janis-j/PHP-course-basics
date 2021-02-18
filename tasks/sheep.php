ca37759777e11dc8444d206995d1fc9a7eef591d
janis-j <jaanis.jankovs@gmail.com>
2021-02-18 11:42:55 +0200

    adding new map tasks

---
+++ tasks/sheep.php
@@ -0,0 +1,18 @@
+<?php
+
+$animals = ['sheep', 'sheep', 'wolf', 'sheep', 'wolf', 'sheep', 'sheep', 'sheep'];
+
+for ($i = 0; $i < count($animals); $i++) {
+    if ($animals[$i] === 'sheep' && $animals[$i+1] === 'wolf') {
+        echo "OMG" . ' ';
+    }
+    else if ($animals[$i] === 'sheep')
+    {
+        echo "Happy" . ' ';
+    }
+    else if ($animals[$i] === 'wolf')
+    {
+        echo "HEHE" . ' ';
+    }
+    PHP_EOL;
+}
